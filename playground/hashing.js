0c36029e5deea38829d82f4183ead25a108c44c9
andrei.nemikin <andrei.nemikin@gmail.com>
2018-01-19 15:08:03 +0200

    Hash password befor save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 4
-};
+var password = '123abs!';
 
-let token = jwt.sign(data, '123abc');
-let decoded = jwt.verify(token, '123abc');
-console.log(token);
-console.log(decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
+
+var hashedPass = '$2a$10$JOrFR78r/M7CR8C/R.ToJuMdQiFVeDaTCgIy1zDB8753UPNOXQR8a';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+    console.log(res);
+});
+
+// let data = {
+//     id: 4
+// };
+//
+// let token = jwt.sign(data, '123abc');
+// let decoded = jwt.verify(token, '123abc');
+// console.log(token);
+// console.log(decoded);
 //
 // let message = "I am user 23";
 //
